cecef97ba5f6176df40ae0f2df649ee5312786b3
rusleysantos <rusley_silva@hotmail.com>
2019-03-31 22:45:31 -0300

    Start

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiestudo110319.Controllers
+{
+    [Route("api/images")]
+    public class ImagesController : Controller
+    {
+
+        public async Task<ActionResult> GetImage(
+           [FromQuery] string name,
+           [FromServices]IHostingEnvironment env
+            )
+        {
+            var filePath = Path.Combine(env.ContentRootPath, "Images", name);
+            var fileExists = System.IO.File.Exists(filePath);
+            if (fileExists == false)
+            {
+                return NotFound();
+            }
+            else
+
+
+
+
+        }
+
+
+    }
+}
