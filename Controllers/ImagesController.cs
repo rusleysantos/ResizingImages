b1c78e1d4d94fc336c086c5762a813dca12bb5a3
rusleysantos <rusley_silva@hotmail.com>
2020-05-22 14:48:55 -0300

    Altera estrutura do projeto

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -3,33 +3,73 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using ImageMagick;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
-namespace apiestudo110319.Controllers
+namespace ResizingImages.Controllers
 {
-    [Route("api/images")]
+    [Route("api")]
     public class ImagesController : Controller
     {
+        public IHostEnvironment Env { get; set; }
 
-        public async Task<ActionResult> GetImage(
-           [FromQuery] string name,
-           [FromServices]IHostingEnvironment env
-            )
+        public ImagesController(IHostEnvironment env)
         {
-            var filePath = Path.Combine(env.ContentRootPath, "Images", name);
+            Env = env;
+        }
+
+        /// <summary>
+        /// Retorna imagem em seu tamanho normal
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("{action}")]
+        public async Task<ActionResult> GetImage([FromQuery] string name)
+        {
+            var filePath = Path.Combine(Env.ContentRootPath, "img", name);
             var fileExists = System.IO.File.Exists(filePath);
+
             if (fileExists == false)
             {
                 return NotFound();
             }
-            else
 
+            var file = await System.IO.File.ReadAllBytesAsync(filePath);
 
-
-
+            return File(file, "image/jpeg");
         }
 
+        /// <summary>
+        /// Faz o resize da imagem
+        /// </summary>
+        /// <param name="name">Arquivo</param>
+        /// <param name="width">Largura</param>
+        /// <param name="height">Altura</param>
+        /// <param name="quality">Qualidade</param>
+        /// <returns></returns>
+        [HttpGet("{action}")]
+        public async Task<ActionResult> ResizeImage([FromQuery] string name, int width, int height, int quality)
+        {
+            var filePath = Path.Combine(Env.ContentRootPath, "img", name);
+            var fileExists = System.IO.File.Exists(filePath);
 
+            if (fileExists == false)
+            {
+                return NotFound();
+            }
+
+            using (var memory = new MemoryStream())
+            using (var image = new MagickImage(filePath))
+            {
+                image.Resize(width, height);
+                image.Strip();
+                image.Quality = quality;
+                image.Write(memory);
+                var file = memory.ToArray();
+
+                return File(file, "image/jpeg");
+            }
+        }
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# resizingimages
-Projeto para aprofundamento nos estudos do ASP.NET Core 2.2
